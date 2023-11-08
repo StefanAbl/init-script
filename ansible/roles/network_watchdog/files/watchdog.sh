a480de6178bca5076b0b2f008a4944618559b316
StefanAbl <st.ablassmeier@protonmail.com>
2023-11-08 17:12:40 +0100

    ansible: update network watchdog role

--- ansible/roles/network_watchdog/files/watchdog.sh
+++ ansible/roles/network_watchdog/files/watchdog.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-test_host=`ip r | grep "default" | awk '{ print $3}' | xargs ping -q -w 1 -c 1 | grep "received" | awk '{ print $4 }'`
+logfile=/var/log/watchdog
+
+if [ $(cat $logfile | wc -l)  -ge 10000 ]; then
+           mv $logfile $logfile.1
+else
+           echo "Logfile small enough"
+fi
+
+date >> $logfile
+echo "Running watchdog" >> $logfile
+
+test_host=`ip r | grep "default" | awk '{ print $3}' | xargs timeout 10 ping -q -w 1 -c 1 | grep "received" | awk '{ print $4 }'`
 if [ "$test_host" == "0" ] || [ -z "$test_host" ] ;
 then
     service networking restart
+    echo "Trying to restart networking" >> $logfile
 
     sleep 60
-    test_host=`ip r | grep "default" | awk '{ print $3}' | xargs ping -q -w 1 -c 1 | grep "received" | awk '{ print $4 }'`
+    test_host=`ip r | grep "default" | awk '{ print $3}' | xargs timeout 10 ping -q -w 1 -c 1 | grep "received" | awk '{ print $4 }'`
     if [ "$test_host" == "0" ] || [ -z "$test_host" ] ;
     then
-            reboot
+        date >> $logfile
+        echo "Rebooting" >> $logfile
+        /usr/sbin/reboot now
+            shutdown --reboot 1 "System rebooting in 1 minute"
+        sleep 120
+        echo "Uh oh" >> $logfile
+    else
+        echo "Networking restored" >> $logfile
     fi
 else
     echo "Network is reachable"
+    echo "Network is reachable" >> $logfile
 fi
